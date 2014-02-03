a4c4d2a7fc0fea493c70b693bfec484e3f3ab51e
Wesam Elshiekh <wesam.elshiekh@icainformatics.com>
2014-02-03 13:08:19 -0600

    Update pickles, doxygen, dotcover and archive

---
+++ doxygen/search/properties_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['maxversions',['MaxVersions',['../class_h_base_1_1_stargate_1_1_client_1_1_config_1_1_config_cell_query.html#a460499517d21195a9a90557b9291c604',1,'HBase.Stargate.Client.Config.ConfigCellQuery.MaxVersions()'],['../class_h_base_1_1_stargate_1_1_client_1_1_models_1_1_cell_query.html#ace0239a1e0c96968221fc9ee2f98de46',1,'HBase.Stargate.Client.Models.CellQuery.MaxVersions()']]],
+  ['mimetype',['MimeType',['../interface_h_base_1_1_stargate_1_1_client_1_1_type_conversion_1_1_i_mime_converter.html#a44d066f37867600c79143884dfb4803f',1,'HBase.Stargate.Client.TypeConversion.IMimeConverter.MimeType()'],['../class_h_base_1_1_stargate_1_1_client_1_1_type_conversion_1_1_xml_mime_converter.html#a930d9c580e251ea4e2557b1202ed99f4',1,'HBase.Stargate.Client.TypeConversion.XmlMimeConverter.MimeType()']]],
+  ['minversions',['MinVersions',['../class_h_base_1_1_stargate_1_1_client_1_1_models_1_1_column_schema.html#a58471ac7cd43ad882a7f1545320e7193',1,'HBase::Stargate::Client::Models::ColumnSchema']]]
+];
